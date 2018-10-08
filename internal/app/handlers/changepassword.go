6ca890d851c82f6b65c071d74b547b1b3f20924e
Lennart Heinrich <lheinrich@lheinrich.de>
2018-10-08 15:01:48 +0200

    changepassword for other users

--- internal/app/handlers/changepassword.go
+++ internal/app/handlers/changepassword.go
@@ -16,9 +16,17 @@ func ChangePassword(conn net.Conn, request map[string]interface{}, username stri
 	var err error
 
 	// get module name and check if provided
-	newPassword := handler.GetString(request, "newPassword")
+	newPassword, changeUsername := handler.GetString(request, "newPassword"), handler.GetString(request, "changeUsername")
 	if newPassword == "" {
 		return errors.New("400")
+	}
+
+	// check permission
+	if changeUsername != "" && !HasPermission(username, "evortexcp.userList.editUser") {
+		return errors.New("403")
+	} else if changeUsername == "" {
+		// set username for password change
+		changeUsername = username
 	}
 
 	// hash password
@@ -29,7 +37,7 @@ func ChangePassword(conn net.Conn, request map[string]interface{}, username stri
 	}
 
 	// update db
-	_, err = db.DB.Exec("UPDATE users SET passwordHash = $1 WHERE username = $2;", string(passwordHash), username)
+	_, err = db.DB.Exec("UPDATE users SET passwordHash = $1 WHERE username = $2;", string(passwordHash), changeUsername)
 	if err != nil {
 		return err
 	}
